6c93891e3d1d1d98ee70cc6d40764827b634a9ce
Wize <ngendahk@gmail.com>
2024-04-29 10:57:15 +0300

    Add random_forests sources to the tree

---
+++ tests/model_tests.cmake
@@ -0,0 +1,16 @@
+# testing executable
+enable_testing()
+add_executable(model_tests)
+target_link_directories(
+  model_tests
+  PUBLIC
+  ${CMAKE_PROJECT_PATH}/decision_forests
+)
+target_link_libraries(
+  model_tests
+  PRIVATE
+  model_tests_obj
+)
+include(GoogleTest)
+gtest_discover_tests(model_tests)
+
