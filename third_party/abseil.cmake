f5486ee143a07580247b933e3725ccd88a0b64de
Wize <ngendahk@gmail.com>
2024-05-07 13:32:38 +0300

    Comment abseil cmake file

--- third_party/abseil.cmake
+++ third_party/abseil.cmake
@@ -1,6 +1,7 @@
 set(ABSL_FIND_GOOGLETEST OFF)
 set(ABSL_USE_EXTERNAL_GOOGLETEST ON)
 set(ABSL_PROPAGATE_CXX_STD ON)
+# https://github.com/protocolbuffers/protobuf/issues/12185
 set(ABSL_ENABLE_INSTALL ON)
 
 FetchContent_Declare(
