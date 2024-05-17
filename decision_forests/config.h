cb073c4d00669e70a26c9c7e473e7da1535e8133
Wize <ngendahk@gmail.com>
2024-05-17 10:21:01 +0300

    Minor formatting to config

--- decision_forests/config.h
+++ decision_forests/config.h
@@ -6,8 +6,7 @@
     defined(_M_X64) || defined(_M_AMD64) || \
     defined(__arm64) || defined(__arm64__) || \
     defined(_M_ARM64) || defined(__aarch64__)
-
-  #define YGGDRASIL_EXAMPLE_IDX_64_BITS
+    #define YGGDRASIL_EXAMPLE_IDX_64_BITS
   #else
     #define YGGDRASIL_EXAMPLE_IDX_32_BITS
   #endif
