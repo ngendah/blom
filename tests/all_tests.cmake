c62899bbcff75bc3fc8fb4baa7f82655f3ddbb84
Wize <ngendahk@gmail.com>
2024-05-18 10:59:23 +0300

    Refactor learner tests by converting its test object to an executable

--- tests/all_tests.cmake
+++ tests/all_tests.cmake
@@ -11,16 +11,9 @@ target_link_libraries(
   PRIVATE
   metric_tests_obj
   serving_tests_obj
-  learner_tests_obj
   model_tests_obj
   model_decision_tree_tests_obj
   model_random_forest_tests_obj
-  learner_cart_tests_obj
-  learner_decision_tree_tests_obj
-  learner_generic_worker_tests_obj
-  learner_hyperparameters_optimizer_tests_obj
-  learner_optimizers_random_tests_obj
-  learner_random_forest_tests_obj
   serving_decision_forest_tests_obj
 )
 if(NOT CMAKE_CROSSCOMPILING)
